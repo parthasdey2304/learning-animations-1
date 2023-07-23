4fc68ea987a16fa2d926cbfbb01462cd077e4ac1
Partha Sarathi Dey <parthasdeyofficial@gmail.com>
2023-07-23 13:52:25 +0530

    initial commit

--- script.js
+++ script.js
@@ -14,3 +14,8 @@ timeline
     opacity: 0
 }, "-=0.5")
 
+let mousepointer = document.querySelector("#mousepointer")
+mousepointer.addEventListener("mousemove", () => {
+    mousepointer.style.top = event.pageX;
+    mousepointer.style.left = event.pageY;
+})

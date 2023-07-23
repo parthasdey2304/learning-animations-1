fe01b5293daff1f8721f1e8909bf26f378c8f001
Partha Sarathi Dey <parthasdeyofficial@gmail.com>
2023-07-23 15:54:00 +0530

    initial commit

--- script.js
+++ script.js
@@ -14,6 +14,30 @@ timeline
     ease: "expo.easeInOut",
     opacity: 0
 }, "-=0.5")
+.from("#box1", {
+    duration: 1,
+    x: -100,
+    ease: "expo.easeInOut",
+    opacity: 0
+})
+.from("#box2", {
+    duration: 1,
+    x: 100,
+    ease: "expo.easeInOut",
+    opacity: 0
+}, "-=0.25")
+.from("#box3", {
+    duration: 1,
+    x: -100,
+    ease: "expo.easeInOut",
+    opacity: 0
+}, "-=0.25")
+.from("#box4", {
+    duration: 1,
+    x: 100,
+    ease: "expo.easeInOut",
+    opacity: 0
+}, "-=0.25")
 
 let mousepointer = document.querySelector("#mousepointer")
 mousepointer.addEventListener("mousemove", () => {

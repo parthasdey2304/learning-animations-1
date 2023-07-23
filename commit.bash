1f46e57146224054c60c708b97a7f80f3fab762a
Partha Sarathi Dey <parthasdeyofficial@gmail.com>
2023-07-23 06:33:34 -0400

    initial commit

--- commit.bash
+++ commit.bash
@@ -1,3 +1,4 @@
-git add .
-git commit -m "initial commit"
-git push
+git status # this shows the list of the files that have been added or updated or removed or modified
+git add . # this adds them to be commited
+git commit -m "initial commit" # this commits the files
+git push # this pushes the code to the main branch of the repository

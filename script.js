a229ed9799c4cae764827f6f7023701f48dbfab1
Partha Sarathi Dey <parthasdeyofficial@gmail.com>
2023-07-23 15:49:41 +0530

    initial commit

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const timeline = gsap.timeline();
 timeline
 .from("#typing", {
     duration: 1,
-    y: 50,
+    y: 70,
     ease: "expo.easeInOut",
     opacity: 0
 })
 .from("#mountains", {
     duration: 1,
     y: 60,
-    scale: 2,
+    scale: 1.5,
     ease: "expo.easeInOut",
     opacity: 0
 }, "-=0.5")
